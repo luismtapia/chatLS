0218ca46f2cf674cd567f91981dc238a3badb830
Nair Flores <14030637@itcelaya.edu.mx>
2019-10-24 11:03:02 -0500

    ya yaya

---
+++ Lynxspace/lynxspace.class.php
@@ -0,0 +1,49 @@
+<?php
+	class LynxSpace{
+		var $conexion = null;
+		function __construct(){
+			
+		}
+		function conexion(){
+			include('config.php');
+			$this->conexion = new PDO($sgbd.':host='.$bdhost.';dbname='.$bdname, $bdusuario, $bdpassword);
+		}
+		function registro($data){
+			$this->conexion($data);
+			$this->conexion->beginTransaction();
+			$fecha = $this->fecha($data['dia'],$data['mes'],$data['anio']);
+			try{
+				$sql = 'INSERT INTO usuario(email, contrasena) VALUES (:email, :contrasena)';
+				$sentencia = $this->conexion->prepare($sql);
+				$sentencia->bindParam(':email', $data['email']);
+
+				$data['contrasena'] = md5($data['contrasena']);
+				$sentencia->bindParam(':contrasena', $data['contrasena']);
+				$sentencia->execute();
+
+				$sql = 'SELECT * FROM usuario WHERE email = :email';
+				$sentencia = $this->conexion->prepare($sql);
+				$sentencia->bindParam(':email', $data['email']);
+				$sentencia->execute();
+				$fila = $sentencia->fetch();
+				
+				$sql = 'INSERT INTO persona(nombre, apellidos, apodo, nacimiento, id_usuario) VALUES (:nombre, :apellidos, :apodo, :nacimiento, :id_usuario)';
+				$sentencia = $this->conexion->prepare($sql);
+				$sentencia->bindValue(':nombre', $data['nombre']);
+				$sentencia->bindValue(':apellidos', $data['apellidos']);
+				$sentencia->bindValue(':apodo', $data['apodo']);
+				$sentencia->bindValue(':nacimiento', $fecha);
+				$sentencia->bindValue(':id_usuario', $fila['id_usuario']);
+				$sentencia->execute();
+				$this->conexion->commit();
+			}catch(Exception $e){
+				$this->conexion->rollBack();
+			}
+		}
+		function fecha($dia, $mes, $anio){
+			$fecha = $anio.'-'.$mes.'-'.$dia;
+			return $fecha;
+		}
+	}
+	$sitio = new LynxSpace;
+?>
