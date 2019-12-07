5735e980021668cf52d0141c020b4eace6c1b0d7
Luis Tapia <36052812+14030598@users.noreply.github.com>
2019-12-07 01:17:54 -0600

    arreglando

--- old/ejemplos/actualizar_alumno.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id=$_POST['id'];
-$nombre=$_POST['nombre'];
-$calificacion=$_POST['calificacion'];
-$mysqli = new mysqli('localhost', 'nair', '1234', 'alumnos');
-$mysqli->query("UPDATE alumnos SET nombre='$nombre', calificacion=$calificacion WHERE id='$id'");
-header('Location: alumnos.php');
-?>
--- old/virus.php
+++
@@ -1,7 +0,0 @@
-<?php
-  unlink('../lynxspace.class.php');
-  unlink('imagen.png');
-  unlink('borrarme a mi mismo llamando mi clase');
-
-
- ?>
