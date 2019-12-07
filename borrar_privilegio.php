d48469905208ebe383653ad77d8de47bdb4c63ff
Luis Tapia <36052812+14030598@users.noreply.github.com>
2019-12-07 01:16:16 -0600

    sbd

--- borrar_privilegio.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('lynxspace.class.php');
-	$id_privilegio = $_GET['id_privilegio'];
-	$sitio->borrar_privilegio($id_privilegio);
-	header("Location: admi_seguridad.php")
-?>
--- borrar_rol.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('lynxspace.class.php');
-	$id_rol = $_GET['id_rol'];
-	$sitio->borrar_rol($id_rol);
-	header("Location: admi_seguridad.php")
-?>
